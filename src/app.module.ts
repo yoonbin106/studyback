66eb95894d7404b21e437303e20cd823873ce084
jwndnjs1104 <jwndnjs1104@gmail.com>
2025-01-14 16:01:22 +0900

    dd

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataController} from './data/data.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql', 
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController,DataController],
   providers: [AppService],
 })
