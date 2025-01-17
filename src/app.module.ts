20e7284269256b97918783b64b55cd1e792a1877
RiaLeee <gyurim26@gmail.com>
2025-01-17 23:27:13 +0900

    zz

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataController} from './data/data.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',  // MySQL을 사용
+      host: 'localhost',  // MySQL 서버 호스트
+      port: 3306,  // MySQL 기본 포트
+      username: 'lgm',  // MySQL 사용자 이름
+      password: 'lgm',  // MySQL 비밀번호
+      database: 'studydb',  // 사용하려는 데이터베이스 이름
+      entities: [],  // 엔티티 파일을 여기에 추가 (예: [User, Post] 등)
+      synchronize: true,  // 자동으로 데이터베이스 구조 동기화
+    }),
+  ],
   controllers: [AppController,DataController],
   providers: [AppService],
 })
