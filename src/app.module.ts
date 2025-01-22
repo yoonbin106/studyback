2d07467b28c94e3c6f72b95583c4912820d48ecf
jwndnjs1104 <jwndnjs1104@gmail.com>
2025-01-22 14:07:46 +0900

    mola

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: 'diznfmxm3!',
       database: 'test',
       entities: [],
       synchronize: true,
